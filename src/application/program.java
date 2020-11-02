19bf4bedd10a29b21559de520f0e4ad21e74a695
anderson-indiano <indianobox2@gmail.com>
2020-11-01 22:12:24 -0300

    Mensagem de boa noite

--- src/application/program.java
+++ src/application/program.java
@@ -5,6 +5,7 @@ public class program {
 	public static void main(String[] args) {
 		System.out.println("Bom dia!");
 		System.out.println("Bom tarde!");
+		System.out.println("Boa noite!");
 	}
 
 }
